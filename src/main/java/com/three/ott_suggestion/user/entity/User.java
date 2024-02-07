c37da86d713cae1ad66d54562b271ee07ef34e3b
yiyaaa <thdtjs1219@gmail.com>
2024-02-07 20:22:48 +0900

    feat: 공동 코드 작성

---
+++ src/main/java/com/three/ott_suggestion/user/entity/User.java
@@ -0,0 +1,32 @@
+package com.three.ott_suggestion.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email" ,nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String nickname;
+
+    @Column
+    private String introduction;
+}
