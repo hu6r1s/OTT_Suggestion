59f296e236ef9d08a8a2d881e1d255643ef91cf8
Hu6r1s <darmaa333@gmail.com>
2024-02-07 17:16:51 +0900

    init commit

---
+++ src/main/java/com/three/ott_suggestion/OttSuggestionApplication.java
@@ -0,0 +1,13 @@
+package com.three.ott_suggestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OttSuggestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OttSuggestionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/three/ott_suggestion/OttSuggestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.three.ott_suggestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OttSuggestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
