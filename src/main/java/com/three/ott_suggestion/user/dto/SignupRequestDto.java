c44908b654d4fba4ff35df251a379b21685cbe10
wooseok <wooseok6008@gmail.com>
2024-02-08 18:57:05 +0900

    feat: 회원조회 기능

--- src/main/java/com/three/ott_suggestion/user/dto/SignupRequestDto.java
+++ src/main/java/com/three/ott_suggestion/user/dto/SignupRequestDto.java
@@ -8,11 +8,11 @@ import lombok.Getter;
 public class SignupRequestDto {
 
     @NotBlank(message = "이메일은 필수 입력 값입니다.")
-    @Pattern(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
+    @Pattern(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", message = "이메일 주소 형식을 지켜주세요")
     private String email;
 
-    @NotBlank(message = "Password는 8자 이상 15자 이하이어야 합니다.")
-    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).*$")
+    @NotBlank(message = "비밀번호를 입력하세요.")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).*$", message = "Password는 8자 이상 15자 이하이어야 합니다.")
     private String password;
 
     private String nickname;
