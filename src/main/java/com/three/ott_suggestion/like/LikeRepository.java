4d690f1135af848253e6f767758f12314af439aa
HEEEUN9812 <heeeun9812@gmail.com>
2024-02-08 23:10:14 +0900

    feat: 좋아요 토글 기능 구현

---
+++ src/main/java/com/three/ott_suggestion/like/LikeRepository.java
@@ -0,0 +1,9 @@
+package com.three.ott_suggestion.like;
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikeRepository extends JpaRepository<Like, Long> {
+
+    Optional<Like> findByUserIdAndPostId(Long userId, Long postId);
+}
