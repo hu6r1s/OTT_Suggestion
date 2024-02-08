c99664ef418a2f95d19f956186d39b71bfad0b2e
jinkshower <askthem@naver.com>
2024-02-08 10:23:30 +0900

    feat: CommonResponse 생성

--- src/main/java/com/three/ott_suggestion/global/exception/ControllerAdvice.java
+++ src/main/java/com/three/ott_suggestion/global/exception/ControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.three.ott_suggestion.global.exception;
 
+import com.three.ott_suggestion.global.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
