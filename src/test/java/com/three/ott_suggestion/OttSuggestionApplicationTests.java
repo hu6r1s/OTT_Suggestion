70c63dd27e2dd1672e1ddb8e04a5e30b7dbd662d
Hu6r1s <darmaa333@gmail.com>
2024-02-21 14:32:32 +0900

    test: setup

--- src/test/java/com/three/ott_suggestion/OttSuggestionApplicationTests.java
+++ src/test/java/com/three/ott_suggestion/OttSuggestionApplicationTests.java
@@ -2,8 +2,10 @@ package com.three.ott_suggestion;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OttSuggestionApplicationTests {
 
     @Test
