17ce2a2fae5902d0cb1b5ce0ceaa6a771b8f8bcb
Hu6r1s <darmaa333@gmail.com>
2024-02-21 14:33:19 +0900

    test(api): login 테스트 구현 안됨

--- src/test/java/com/three/ott_suggestion/auth/AuthControllerTest.java
+++ src/test/java/com/three/ott_suggestion/auth/AuthControllerTest.java
@@ -1,6 +1,8 @@
 package com.three.ott_suggestion.auth;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -10,7 +12,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.three.ott_suggestion.auth.filter.MockSpringSecurityFilter;
 import com.three.ott_suggestion.global.config.SecurityConfig;
+import com.three.ott_suggestion.global.filter.JwtAuthenticationFilter;
+import com.three.ott_suggestion.global.util.JwtUtil;
+import com.three.ott_suggestion.global.util.UserDetailsImpl;
+import com.three.ott_suggestion.global.util.UserDetailsServiceImpl;
 import com.three.ott_suggestion.user.controller.AuthController;
+import com.three.ott_suggestion.user.entity.User;
+import com.three.ott_suggestion.user.repository.RefreshTokenRepository;
 import com.three.ott_suggestion.user.service.AuthService;
 import java.security.Principal;
 import java.util.HashMap;
@@ -18,12 +26,14 @@ import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -37,7 +47,7 @@ import org.springframework.web.context.WebApplicationContext;
         )
     }
 )
-public class AuthTest {
+public class AuthControllerTest {
 
     private MockMvc mvc;
     private Principal mockPrincipal;
@@ -58,12 +68,22 @@ public class AuthTest {
             .build();
     }
 
+    private void mockUserSetup() {
+        String email = "test@test.com";
+        String password = "ASDasd123!!!";
+        String nickname = "test";
+        String introduction = "test";
+        User testUser = new User(email, password, nickname, introduction, null, true);
+        UserDetailsImpl testUserDetails = new UserDetailsImpl(testUser);
+        mockPrincipal = new UsernamePasswordAuthenticationToken(testUserDetails, null, null);
+    }
+
     @Test
     @DisplayName("회원가입 성공")
     void 회원가입_성공() throws Exception {
         // given
         Map<String, Object> signupRequest = new HashMap<>();
-        signupRequest.put("email", "darmaa333@gmail.com");
+        signupRequest.put("email", "test@test.com");
         signupRequest.put("password", "ASDasd123!!!");
         signupRequest.put("nickname", "test");
         signupRequest.put("introduction", "test");
@@ -71,7 +91,7 @@ public class AuthTest {
         // when - then
         mvc.perform(post("/auth/signup")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsBytes(signupRequest))
+                .content(objectMapper.writeValueAsString(signupRequest))
             )
             .andExpect(status().isOk())
             .andExpect(jsonPath("$.message").value("회원가입 성공"))
@@ -91,7 +111,7 @@ public class AuthTest {
         // when - then
         mvc.perform(post("/auth/signup")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsBytes(signupRequest))
+                .content(objectMapper.writeValueAsString(signupRequest))
             )
             .andExpect(status().isBadRequest())
             .andExpect(jsonPath("$.message").value("회원가입 실패"))
@@ -99,6 +119,43 @@ public class AuthTest {
                 "이메일 주소 형식을 지켜주세요",
                 "비밀번호는 8글자~20자, 대문자 1개, 소문자 1개, 숫자 1개, 특수문자 1개 이상 포함하세요."
             )))
+            .andDo(print());
+    }
+
+    @Test
+    @DisplayName("로그인 성공")
+    void 로그인_성공() throws Exception {
+        // given
+        mockUserSetup();
+        Map<String, Object> signupRequest = new HashMap<>();
+        signupRequest.put("email", "test@test.com");
+        signupRequest.put("password", "ASDasd123!!!");
+
+        // when - then
+        mvc.perform(post("/auth/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(signupRequest))
+            )
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.message").value("로그인에 성공하였습니다."))
+            .andDo(print());
+    }
+
+    @Test
+    @DisplayName("로그인 실패")
+    void 로그인_실패() throws Exception {
+        // given
+        String email = "darmaa333@gmai.com";
+        String password = "ASDasd123!!!";
+
+        // when - then
+        mvc.perform(post("/auth/login")
+                .param("email", email)
+                .param("passowrd", password)
+                .with(csrf())
+            )
+            .andExpect(status().isBadRequest())
+            .andExpect(jsonPath("$.message").value("로그인에 실패하였습니다."))
             .andDo(print());
     }
 }
