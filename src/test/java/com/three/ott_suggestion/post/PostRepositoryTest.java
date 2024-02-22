6ad91218bd65348fad2256e3a27af4cfa99084a1
Hu6r1s <darmaa333@gmail.com>
2024-02-22 15:18:09 +0900

    test(api): 게시글 레포지토리 테스트

--- src/test/java/com/three/ott_suggestion/post/PostRepositoryTest.java
+++ src/test/java/com/three/ott_suggestion/post/PostRepositoryTest.java
@@ -9,6 +9,7 @@ import com.three.ott_suggestion.user.entity.User;
 import com.three.ott_suggestion.user.repository.UserRepository;
 import java.util.List;
 import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +20,13 @@ public class PostRepositoryTest {
     @Autowired
     private PostRepository postRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     private Post post;
     private User user;
 
+    @BeforeEach
     private void mockPostSetup() {
         String title = "test title";
         String content = "test content";
@@ -31,21 +36,51 @@ public class PostRepositoryTest {
         String introduction = "test";
         user = new User(email, password, nickname, introduction, null, true);
         post = new Post(new PostRequestDto(title, content), user);
+        userRepository.save(user);
         postRepository.save(post);
-
     }
     @Test
     @DisplayName("findByUserId 테스트")
     void findByUserIdTest() {
         // given
-        mockPostSetup();
-
         // when
-        List<Post> selectPost = postRepository.findByUserId(post.getUser().getId());
+        List<Post> selectPost = postRepository.findByUserId(1L);
 
         // then
         assertEquals(post.getTitle(), selectPost.get(0).getTitle());
     }
 
+    @Test
+    @DisplayName("findByTitleContains 테스트")
+    void findByTitleContainsTest() {
+        // given
+        // when
+        List<Post> selectPost = postRepository.findByTitleContains(post.getTitle());
 
+        // then
+        assertEquals(post.getTitle(), selectPost.get(0).getTitle());
+    }
+
+    @Test
+    @DisplayName("findPostByIdAndDeletedAtIsNull 테스트")
+    void findPostByIdAndDeletedAtIsNullTest() {
+        // given
+        // when
+        Optional<Post> selectPost = postRepository.findPostByIdAndDeletedAtIsNull(post.getId());
+
+        // then
+        assertEquals(post.getTitle(), selectPost.get().getTitle());
+    }
+
+    @Test
+    @DisplayName("findAllByDeletedAtIsNullOrderByCreatedAtDesc 테스트")
+    void findAllByDeletedAtIsNullOrderByCreatedAtDescTest() {
+        // given
+        // when
+        List<Post> selectPost = postRepository
+            .findAllByDeletedAtIsNullOrderByCreatedAtDesc();
+
+        // then
+        assertEquals(post.getTitle(), selectPost.get(0).getTitle());
+    }
 }
